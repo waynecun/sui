ddea9716071e1994e5a9248b801f9ec6d9bb6e62
Andrew Burnie <apburnie@hotmail.co.uk>
2022-09-30 19:22:03 +0100

    implements placeholder loading page

--- apps/explorer/src/components/transaction-card/TxCardUtils.tsx
+++ apps/explorer/src/components/transaction-card/TxCardUtils.tsx
@@ -70,7 +70,13 @@ export const genTableDataFromTxData = (
 ) => {
     return {
         data: results.map((txn) => ({
-            date: `${timeAgo(txn.timestamp_ms, undefined, true)} ago`,
+            date: (
+                <div
+                    style={{
+                        width: '85px',
+                    }}
+                >{`${timeAgo(txn.timestamp_ms, undefined, true)} ago`}</div>
+            ),
             transactionId: [
                 {
                     url: txn.txId,
@@ -134,44 +140,6 @@ export const genTableDataFromTxData = (
             },
         ],
     };
-};
-
-// Generate loading table display
-export const loadingTable = {
-    data: new Array(15).fill({
-        date: <div className={styles.placeholder} />,
-        txTypes: <div className={styles.placeholder} />,
-        transactionId: <div className={styles.placeholder} />,
-        addresses: <div className={styles.placeholder} />,
-        amounts: <div className={styles.placeholder} />,
-        gas: <div className={styles.placeholder} />,
-    }),
-    columns: [
-        {
-            headerLabel: 'Time',
-            accessorKey: 'date',
-        },
-        {
-            headerLabel: 'Type',
-            accessorKey: 'txTypes',
-        },
-        {
-            headerLabel: 'Transaction ID',
-            accessorKey: 'transactionId',
-        },
-        {
-            headerLabel: 'Addresses',
-            accessorKey: 'addresses',
-        },
-        {
-            headerLabel: 'Amount',
-            accessorKey: 'amounts',
-        },
-        {
-            headerLabel: 'Gas',
-            accessorKey: 'gas',
-        },
-    ],
 };
 
 export const getDataOnTxDigests = (
